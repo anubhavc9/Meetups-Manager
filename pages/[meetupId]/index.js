6cfed97787e52d0c49f034f7d250b226bd2e0ed1
anubhavc9 <anubhavc9@github.com>
2022-02-11 10:24:02 +0530

    initial commit

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image='https://media-cdn.tripadvisor.com/media/photo-s/01/1b/ae/57/another-view.jpg'
+      title='First Meetup'
+      address='Chicago, Ilinois'
+      description='This is a First Meetup'
+    />
+  );
+}
+
+export default MeetupDetails;
---
+++ pages/index.js
@@ -0,0 +1,36 @@
+// our-domain.com/
+
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://media-cdn.tripadvisor.com/media/photo-s/01/1b/ae/57/another-view.jpg',
+    address: 'Chicago, Ilinois',
+    description: 'This is a first meetup among the fans',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://businessfacilities.com/wp-content/uploads/2019/05/AtlantaGA-571x300.jpg',
+    address: 'Atlanta, Georgia',
+    description: 'This is a second meetup among the fans',
+  },
+  {
+    id: 'm1',
+    title: 'A Third Meetup',
+    image:
+      'https://www.visittheusa.com/sites/default/files/styles/hero_l/public/images/hero_media_image/2016-10/0%20HERO_HoustonTX_GettyImages-532390052.jpg?h=c5520b1b&itok=uKyYtaMD',
+    address: 'Houston, Texas',
+    description: 'This is a third meetup among the fans',
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
