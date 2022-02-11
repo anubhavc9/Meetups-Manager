44af02aa20b41b07a89fd135d3ced6ecd92c87e9
anubhavc9 <anubhavc9@github.com>
2022-02-11 16:04:12 +0530

    working with params for SSG data fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,45 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm3',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  // fetch data for a particular meetup with meetupId
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://media-cdn.tripadvisor.com/media/photo-s/01/1b/ae/57/another-view.jpg',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Chicago, Ilinois',
+        description: 'This is a First Meetup',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
