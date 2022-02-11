09e5a1b8a1ed596a3dd2b670305885f1187a61f8
anubhavc9 <anubhavc9@github.com>
2022-02-11 22:00:09 +0530

    getting all meetups data from the db

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,34 @@
 // our-domain.com/
 
 import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image:
-      'https://media-cdn.tripadvisor.com/media/photo-s/01/1b/ae/57/another-view.jpg',
-    address: 'Chicago, Ilinois',
-    description: 'This is a first meetup among the fans',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image:
-      'https://businessfacilities.com/wp-content/uploads/2019/05/AtlantaGA-571x300.jpg',
-    address: 'Atlanta, Georgia',
-    description: 'This is a second meetup among the fans',
-  },
-  {
-    id: 'm3',
-    title: 'A Third Meetup',
-    image:
-      'https://www.visittheusa.com/sites/default/files/styles/hero_l/public/images/hero_media_image/2016-10/0%20HERO_HoustonTX_GettyImages-532390052.jpg?h=c5520b1b&itok=uKyYtaMD',
-    address: 'Houston, Texas',
-    description: 'This is a third meetup among the fans',
-  },
-];
+import { MongoClient } from 'mongodb';
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
 export async function getStaticProps() {
-  // fetch data from an API here. for now, we'll work with DUMMY_MEETUPS
+  const client = await MongoClient.connect(
+    'mongodb+srv://Anubhav123:Anubhav123@cluster0.sdhjc.mongodb.net/meetups?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray(); // retrieve all documents form the 'meetups' collection
+
+  client.close();
 
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
-    revalidate: 100,
+    revalidate: 10,
   };
 }
 
