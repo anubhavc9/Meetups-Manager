b7f5fe1de54158553396163f08de7055f506fe7d
Anubhav <anubhavc9@gmail.com>
2022-04-18 16:08:43 +0530

    Made navbar responsive Added collapsed navbar

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,21 +1,55 @@
 import classes from './MainNavigation.module.css';
 import Link from 'next/link';
+import { useState } from 'react';
+import { FaBars } from "react-icons/fa";
 
 function MainNavigation() {
+  const [collapsedNavbar, setCollapsedNavbar] = useState(false);
+
+  function collapseHandler() {
+    setCollapsedNavbar(prevValue => {
+      return !prevValue;
+    });
+  }
+
   return (
-    <header className={classes.header}>
-      <div className={classes.logo}>Meetups Manager</div>
-      <nav>
-        <ul>
-          <li>
-            <Link href='/'>All Meetups</Link>
-          </li>
-          <li>
-            <Link href='/new-meetup'>Add New Meetup</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div>
+      
+      <header className={classes.header}>
+        <div className={classes.logo}>Meetups Manager</div>
+        <nav>
+          <div className={classes.menuIconWrapper}>
+            <ul>
+              <li>
+                <Link href='/'>All Meetups</Link>
+              </li>
+              <li>
+                <Link href='/new-meetup'>Add New Meetup</Link>
+              </li>
+            </ul>
+            <span onClick={collapseHandler}>
+              <FaBars className={classes.barsIcon} />
+            </span>
+          </div>
+        </nav>
+      </header>
+
+      {collapsedNavbar && <div>
+        <header className={`${classes.header} ${classes.collapsedHeader}`}>
+          <nav>
+            <ul className={classes.collapsedUl}>
+              <li>
+                <Link href='/'>All Meetups</Link>
+              </li>
+              <li>
+                <Link href='/new-meetup'>Add New Meetup</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>}
+
+    </div>
   );
 }
 
