6f9532bf3f1644e1065231dc2f61e74fc02d33cb
Anubhav <anubhavc9@gmail.com>
2022-04-18 12:56:25 +0530

    Added form validation for adding a new meetup

--- components/meetups/NewMeetupForm.js
+++ components/meetups/NewMeetupForm.js
@@ -1,9 +1,18 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
+const isEmpty = (value) => value.trim() === '';
+
 function NewMeetupForm(props) {
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    title: true,
+    image: true,
+    address: true,
+    description: true,
+  });
+
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -17,6 +26,24 @@ function NewMeetupForm(props) {
     const enteredAddress = addressInputRef.current.value;
     const enteredDescription = descriptionInputRef.current.value;
 
+    const enteredTitleIsValid = !isEmpty(enteredTitle);
+    const enteredImageIsValid = !isEmpty(enteredImage);
+    const enteredAddressIsValid = !isEmpty(enteredAddress);
+    const enteredDescriptionIsValid = !isEmpty(enteredDescription);
+
+    setFormInputsValidity({
+      title: enteredTitleIsValid,
+      image: enteredImageIsValid,
+      address: enteredAddressIsValid,
+      description: enteredDescriptionIsValid,
+    });
+
+    const formIsValid = enteredTitleIsValid && enteredImageIsValid && enteredAddressIsValid && enteredDescriptionIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
     const meetupData = {
       title: enteredTitle,
       image: enteredImage,
@@ -27,29 +54,37 @@ function NewMeetupForm(props) {
     props.onAddMeetup(meetupData);
   }
 
+  const titleControlClasses = `${classes.control} ${formInputsValidity.title ? '' : classes.invalid}`;
+  const imageControlClasses = `${classes.control} ${formInputsValidity.image ? '' : classes.invalid}`;
+  const addressControlClasses = `${classes.control} ${formInputsValidity.address ? '' : classes.invalid}`;
+  const descriptionControlClasses = `${classes.control} ${formInputsValidity.description ? '' : classes.invalid}`;
+
   return (
     <Card>
       <form className={classes.form} onSubmit={submitHandler}>
-        <div className={classes.control}>
+        <div className={titleControlClasses}>
           <label htmlFor='title'>Meetup Title</label>
-          <input type='text' required id='title' ref={titleInputRef} />
+          <input type='text' id='title' ref={titleInputRef} />
+          {!formInputsValidity.title && <p>Title cannot be empty!</p>}
         </div>
-        <div className={classes.control}>
+        <div className={imageControlClasses}>
           <label htmlFor='image'>Meetup Image</label>
-          <input type='url' required id='image' ref={imageInputRef} />
+          <input type='url' id='image' ref={imageInputRef} />
+          {!formInputsValidity.image && <p>Please enter a valid image URL!</p>}
         </div>
-        <div className={classes.control}>
+        <div className={addressControlClasses}>
           <label htmlFor='address'>Address</label>
-          <input type='text' required id='address' ref={addressInputRef} />
+          <input type='text' id='address' ref={addressInputRef} />
+          {!formInputsValidity.address && <p>Address cannot be empty!</p>}
         </div>
-        <div className={classes.control}>
+        <div className={descriptionControlClasses}>
           <label htmlFor='description'>Description</label>
           <textarea
             id='description'
-            required
             rows='5'
             ref={descriptionInputRef}
           ></textarea>
+          {!formInputsValidity.title && <p>Description cannot be empty!</p>}
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
